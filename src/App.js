b008e634433ce0462323d5ba4ce60d9dfd96983b
jaaberg <aaberg89@gmail.com>
2016-04-01 18:03:41 +0200

    WIP: Mentions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import PlainTextEditor from './tasks/1/PlainTextEditor.js';
 import EditorWithHashtags from './tasks/3/EditorWithHashtags.js';
+import EditorWithMentions from './tasks/4/EditorWithMentions.js';
 
 export class App extends Component {
   render() {
     return (
       <div>
-        <EditorWithHashtags />
+        <EditorWithMentions />
       </div>
     );
   }
---
+++ src/tasks/4/EditorWithMentions.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import {Editor, EditorState, CompositeDecorator} from 'draft-js';
+import findWithRegex from '../../utils/findWithRegex';
+
+const HANDLE_REGEX = /\@[\w]+/g;
+
+function handleStrategy(contentBlock, callback) {
+  findWithRegex(HANDLE_REGEX, contentBlock, callback);
+}
+
+const NAMES = ['Martin', 'Marte', 'Jørgen', 'Brynjar', 'Henrik', 'Martinus', 'Markus', 'Ole', 'Rune'];
+
+class EditorWithMentions extends Component {
+  constructor(props) {
+    super(props);
+
+    const Mention = (props) => {
+      const filteredNames = NAMES
+          .filter((name) => name.indexOf(props.decoratedText.slice(1)) > -1)
+          .map((name) => <div>{name}</div>);
+
+      return (
+        <span style={styles.mention}>
+          <span {...props}>{props.children}</span>
+          <div style={styles.mentionSuggestions}>
+            {filteredNames}
+          </div>
+        </span>
+      );
+    };
+
+    const compositeDecorator = new CompositeDecorator([
+      {
+        strategy: handleStrategy,
+        component: Mention
+      }
+    ]);
+
+    this.state = {
+      editorState: EditorState.createEmpty(compositeDecorator)
+    };
+
+    this.onChange = (editorState) => this.setState({editorState});
+    this.logState = () => console.log(this.state.editorState.toJS());
+  }
+
+  render() {
+    return (
+      <div style={styles.root}>
+        <div style={styles.editor} onClick={this.focus}>
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+            />
+        </div>
+        <input
+          onClick={this.logState}
+          style={styles.button}
+          type="button"
+          value="Log State"
+          />
+      </div>
+    );
+  }
+}
+
+const styles = {
+  root: {
+    padding: 20,
+    width: 600
+  },
+  editor: {
+    border: '1px solid #ccc',
+    cursor: 'text',
+    padding: 10
+  },
+  button: {
+    marginTop: 10,
+    textAlign: 'center'
+  },
+  mention: {
+    position: 'relative'
+  },
+  mentionSuggestions: {
+    border: '1px solid grey',
+    display: 'inline-block',
+    position: 'absolute',
+    minWidth: '240px',
+    borderRadius: '4px',
+    backgroundColor: 'white',
+    padding: '5px',
+    boxShadow: '0px 4px 30px 0px rgba(220,220,220,1)',
+    top: '20px',
+    left: '0'
+  }
+};
+
+
+export default EditorWithMentions;
